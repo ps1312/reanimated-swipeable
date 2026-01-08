046a9051472436aa52f3d22afe3953eb0be3bd63
ps1312 <ps131296@gmail.com>
2026-01-08 14:59:09 -0300

    add the simplest snap to the left when crossing SNAP_OPEN threshold (30% of screen)

--- App.tsx
+++ App.tsx
@@ -1,20 +1,68 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Dimensions, StyleSheet, View } from 'react-native'
+import {
+  Gesture,
+  GestureDetector,
+  GestureHandlerRootView,
+} from 'react-native-gesture-handler'
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from 'react-native-reanimated'
+
+const SCREEN_WIDTH = Dimensions.get('screen').width
+const SNAP_OPEN = SCREEN_WIDTH * 0.3
 
 export default function App() {
+  const isPressed = useSharedValue(false)
+  const translateX = useSharedValue(0)
+
+  const gesture = Gesture.Pan()
+    .onUpdate((e) => {
+      translateX.value = e.translationX
+    })
+    .onFinalize(() => {
+      if (translateX.value < -SNAP_OPEN) {
+        translateX.value = withSpring(-SNAP_OPEN)
+      } else {
+        translateX.value = withSpring(0)
+      }
+    })
+
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: translateX.value }],
+      backgroundColor: isPressed.value ? 'gray' : 'lightgray',
+    }
+  })
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+    <GestureHandlerRootView>
       <StatusBar style="auto" />
-    </View>
-  );
+
+      <View style={styles.container}>
+        <GestureDetector gesture={gesture}>
+          <Animated.View style={[styles.ball, animatedStyles]} />
+        </GestureDetector>
+      </View>
+    </GestureHandlerRootView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+
+  ball: {
+    width: '100%',
+    height: 100,
+    borderRadius: 16,
+    backgroundColor: 'blue',
+    alignSelf: 'center',
+  },
+})
