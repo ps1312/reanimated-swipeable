2557b1e3a9537f579e85b04f2442fcb9a2d693cb
ps1312 <ps131296@gmail.com>
2026-01-09 14:25:32 -0300

    fix gesture being too sensitive, preventing flatlist to scroll properly

--- components/list-item.tsx
+++ components/list-item.tsx
@@ -1,4 +1,4 @@
-import { Dimensions, StyleSheet, Text } from 'react-native'
+import { Dimensions, StyleSheet } from 'react-native'
 import { Gesture, GestureDetector } from 'react-native-gesture-handler'
 import Animated, {
   useAnimatedStyle,
